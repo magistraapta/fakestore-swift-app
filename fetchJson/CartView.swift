d8ba14601674c3eda30d2b404e76613ea87e64d3
magistraapta <magsitraagis@gmail.com>
2023-12-31 14:49:30 +0800

    add custom back button

--- fetchJson/CartView.swift
+++ fetchJson/CartView.swift
@@ -10,31 +10,67 @@ import SwiftUI
 struct CartView: View {
 //    @StateObject var cartViewModel = CartViewModel()
     @EnvironmentObject var network: NetworkManager
+    @Environment(\.presentationMode) var presentationMode
     var body: some View {
-        List {
-            ForEach(network.cartItem){item in
-                HStack{
-                    AsyncImage(url: URL(string: item.image)) { image in
-                        image
-                            .resizable()
-                            .aspectRatio(contentMode: .fill)
-                            .frame(width: 50, height: 50, alignment: .top)
-                            .background(Color.gray.opacity(0.4))
-                            .clipped()
-                    } placeholder: {
-                        ProgressView()
+        VStack {
+            HStack {
+                Button {
+                    presentationMode.wrappedValue.dismiss()
+                } label: {
+                    ZStack{
+                        Rectangle()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 36)
+                            .cornerRadius(12)
+                            .background()
+                            .foregroundColor(Color.gray.opacity(0.3))
+                        Image(systemName: "chevron.left")
+                            .foregroundColor(Color.black)
+
                     }
-                    Spacer()
-                    Text("\(item.title)")
-                    
+                                
                 }
+                Spacer()
+                
             }
-            .onDelete(perform: network.deleteCartItem)
+            .padding(.horizontal, 15)
+            List {
+                ForEach(network.cartItem){item in
+                    HStack{
+                        AsyncImage(url: URL(string: item.image)) { image in
+                            image
+                                .resizable()
+                                .aspectRatio(contentMode: .fill)
+                                .frame(width: 50, height: 50, alignment: .top)
+                                .background(Color.gray.opacity(0.4))
+                                .clipped()
+                        } placeholder: {
+                            ProgressView()
+                        }
+                        Spacer()
+                        Text("\(item.title)")
+                        
+                    }
+                }
+                .onDelete(perform: network.deleteCartItem)
+            }
+            .listStyle(.plain)
+            .navigationBarBackButtonHidden(true)
         }
-        .navigationTitle("Cart")
-        .navigationBarTitleDisplayMode(.inline)
     }
 }
+
+private struct HeaderView: View {
+    var body: some View{
+        HStack{
+            Image(systemName: "chevron.left")
+                .font(.title2)
+            Spacer()
+        }
+        .padding(.horizontal,15)
+    }
+}
+
 
 struct CartView_Previews: PreviewProvider {
     static var previews: some View {
